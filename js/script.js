d633db7f9c6b6fab1f017fe12ee2c092a2accda9
ViniciusM15IFC <142498596+ViniciusM15IFC@users.noreply.github.com>
2023-11-28 14:11:10 +0000

    Pronto

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const preços = [13.99, 29.99, 19.99, 17.99, 25.99, 49.99, 2, 50, 100]
+const preços = [13.99, 29.99, 19.99, 29.99, 25.99, 49.99, 2, 50, 100]
 const nomes = ["Revolução dos Bichos", "Sherlock Holmes", "Isaac Newton", "Diário de um Banana", "Expresso do Oriente", "David Copperfield", "marcador", "caneta", "organizador"]
 function local() {
     if (localStorage.getItem("hasCodeRunBefore") === null) {
@@ -8,7 +8,10 @@ function local() {
         localStorage.setItem("Diário de um Banana", 0)
         localStorage.setItem("Expresso do Oriente", 0)
         localStorage.setItem("David Copperfield", 0)
-        localStorage.setItem("hasCodeRunBefore", true);
+        localStorage.setItem("marcador", 0)
+        localStorage.setItem("caneta", 0)
+        localStorage.setItem("organizador", 0)
+        localStorage.setItem("hasCodeRunBefore", true)
     }
   }
 function Redirecionar(name) {
@@ -27,8 +30,8 @@ function Comprar(n) {
     sessionStorage.setItem("Nome", nome)
     sessionStorage.setItem("Preço", preço)
     sessionStorage.setItem("Quantidade", quantidade)
+    sessionStorage.setItem("Preço Total", preço * quantidade)
     Redirecionar('form.html')
-    Contar(nome)
 }
 function Info() {
     var nome = sessionStorage.getItem("Nome")
@@ -44,7 +47,8 @@ function Info() {
 }
 function Contar(nome) {
     var num = parseInt(localStorage.getItem(nome))
-    var num = num + 1
+    var qtd = parseFloat(sessionStorage.getItem("Quantidade"))
+    var num = num + qtd
     localStorage.setItem(nome, num)
 }
 function MudarValor(n) {
@@ -62,10 +66,44 @@ function validarEmail() {
     if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
         alert("Email inválido")
         return false
-    }else{
+    }
+    else {
         return true
     }
 }
 function submitForm() {
-    
+    var produto = sessionStorage.getItem("Nome")
+    var preço = sessionStorage.getItem("Preço")
+    var quantidade = sessionStorage.getItem("Quantidade")
+    var cliente = document.getElementById("name").value
+    var valor = preço * quantidade
+    var texto = cliente + ' comprou ' + quantidade + ' ' + produto + ' por $' + valor
+    var elemento = document.getElementById("texto")
+    elemento.innerHTML = texto
+    Contar(produto)
+}
+function quantidadeVendas() { 
+    let soma = 0
+    for ( let index = 0; index < nomes.length; index++) {
+        let nome = nomes[index]
+        let venda = parseInt(localStorage.getItem(nome))
+        soma += venda
+    }
+    alert('A Quantidade de itens vendidos foi ' + soma)
+}
+function valorVendas() {
+    var vendas = []
+    let soma = 0
+    for ( let index = 0; index < nomes.length; index++) {
+        let nome = nomes[index]
+        let venda = parseInt(localStorage.getItem(nome))
+        vendas.push(venda)
+    }
+    for ( let index = 0; index < nomes.length; index++) {
+        let qtd = parseInt(vendas[index])
+        let preço = parseFloat(preços[index])
+        let num = qtd * preço
+        soma += num
+    }
+    alert('O total das vendas foi $' + soma)
 }
